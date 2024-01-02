74286e8f5d14cd55f1a6be6c85fd856329f0909d
JoeMolinelli <116307221+TheGoldDiamond9@users.noreply.github.com>
2024-01-01 20:40:03 -0500

    Add files via upload

---
+++ webroot/js/secrets.js
@@ -0,0 +1,2 @@
+var api_key = 'e1f10a1e78da46f5b10a1e78da96f525'    
+var map_key = 'pk.eyJ1Ijoiam9lbW9saW5lbGxpIiwiYSI6ImNsZnBub2I1NDAwamMzcHJyYWRsaHBmcWoifQ.zpN0aa35qN9MQzouJFhjJA'
---
+++ webroot/js/traffic.js
@@ -0,0 +1,17 @@
+
+
+/*location pull
+var maincitycoords = {name:"",lat:"",lon:""}, marinelocation,
+locList = [], citySlideList = [], state, ccTickerCitiesList = [];*/
+
+
+
+
+let thingTopThingText = "Incident Top";
+let thingTopImpactText = "HIGH IMPACT";
+let thingTopIncidentDesc = " - Incident Description Top";
+let thingBotThingText = "Incident Bottom";
+let thingBotImpactText = "LOW IMPACT";
+let thingBotIncidentDesc = " - Incident Description Bottom";
+let imp1 = "1";
+
